3a048ae4df667c641641761226ec55a28a044f82
ShubhenduADutta <shubhendu@flexli.in>
2023-11-18 10:41:46 +0530

    Create view for serializer.

--- mysite/movies/views.py
+++ mysite/movies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import MovieSerializer
+from .models import MovieData
 # Create your views here.
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = MovieData.objects.all().order_by('name')
+    serializer_class = MovieSerializer
