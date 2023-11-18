b4c76f945ecae5b2277fea6a46d414bd02eaa1de
ShubhenduADutta <shubhendu@flexli.in>
2023-11-18 11:01:03 +0530

    Add genre to movie_data model

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -7,6 +7,7 @@ class MovieData(models.Model):
     name = models.CharField(max_length=200)
     duration = models.FloatField()
     rating = models.FloatField()
+    genre = models.CharField(max_length=500, default="action")
 
     def __str__(self):
         return self.name
